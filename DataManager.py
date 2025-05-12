d5198d02ea48a1c20704f997f2326bfdcbcda16f
frxxn18 <franpado8@gmail.com>
2025-05-12 20:11:42 +0200

    gestor_datos

--- DataManager.py
+++ DataManager.py
@@ -2,21 +2,44 @@ import os
 import json
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, 'documentacion')
+DATA_DIR = os.path.join(BASE_DIR, 'data')
 
-@staticmethod
-def pasar_json(objetos):
-    serializable = {}
+class DataManager:
 
-    if isinstance(objetos, dict):
-        for key, value in objetos.items():
-            clave = str(key)
-            if hasattr(value, 'to_dict'):
-                valor = value.to_dict()
-            else:
-                valor = value
-            serializable[clave] = valor
-    else:
-        serializable = objetos  # Si no es dict, lo dejamos como está
 
-    return json.dumps(serializable, indent=4)
+    @staticmethod
+    def pasar_json(objetos):
+        serializable = {}
+
+        if isinstance(objetos, dict):
+            for key, value in objetos.items():
+                clave = str(key)
+                if hasattr(value, 'to_dict'):
+                    valor = value.to_dict()
+                else:
+                    valor = value
+                serializable[clave] = valor
+        else:
+            serializable = objetos  # Si no es dict, lo dejamos como está
+
+        return json.dumps(serializable, indent=4)
+
+
+
+    @staticmethod
+    def guardar(diccionario, ruta):
+        composed_path = os.path.join(DATA_DIR, ruta + ".json")
+        os.makedirs(os.path.dirname(composed_path), exist_ok=True)
+        with open(composed_path, 'w', encoding='utf-8') as f:
+            f.write(DataManager.pasar_json(diccionario))
+        print("Datos guardados en el archivo", ruta)
+        input("Presiona Enter para continuar... \n")
+
+    @staticmethod
+    def cargar(ruta):
+        composed_path = os.path.join(DATA_DIR, ruta + ".json")
+        datos = {}
+        if os.path.exists(composed_path):
+            with open(composed_path, 'r', encoding='utf-8') as f:
+                datos = json.loads(f.read())
+        return datos
