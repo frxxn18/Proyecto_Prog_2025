7386fd62662b649d4d3ca5f89994aecc1819a392
frxxn18 <franpado8@gmail.com>
2025-05-13 19:12:16 +0200

    autentificacion

--- utils/auth.py
+++ utils/auth.py
@@ -1,1 +1,19 @@
 #aqui iran funciones para el login y el logout
+
+USUARIO = "fran"
+CONTRASEÑA = "123"
+
+def login():
+    print("--Inicio de sesion--")
+    usuario = input("Ingrese el usuario: ").strip()
+    constraseña = input("Ingrese la contraseña: ").strip()
+    if usuario == USUARIO and constraseña == CONTRASEÑA:
+        print("Sesion iniciada")
+        return True
+    else:
+        print("Usuario o contraseña incorrectos")
+        return False
+    
+
+def logout():
+    print("Se ha cerrado la sesion correctamente")
