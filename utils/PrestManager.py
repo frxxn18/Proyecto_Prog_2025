5f491479b0686f484dabbfa189e9444cc08de063
frxxn18 <franpado8@gmail.com>
2025-05-19 09:17:08 +0200

    prestmanager

--- utils/PrestManager.py
+++ utils/PrestManager.py
@@ -3,6 +3,8 @@
 from models.prestamo import Prestamo
 from utils.DataManager import DataManager
 from utils.verificadores import verificar_fecha
+from utils.validadores import validar_nie, validar_isbn
+import os
 
 def registrar_prestamo():
     nie = input("NIE del alumno: ").strip().upper()
@@ -63,4 +65,139 @@ def devolver_libro():
     else:
         print("No se encontró un préstamo activo con ese NIE e ISBN.")
 
+    input("Pulsa Enter para continuar.")
+
+
+def modificar_prestamo():
+    prestamos = DataManager.cargar("prestamos")
+    nie = input("NIE del alumno: ").strip().upper()
+    if not validar_nie(nie):
+        print("NIE no válido")
+        input("Pulsa Enter para continuar.")
+        return
+    
+    isbn = input("ISBN del libro: ").strip()
+    if not validar_isbn(isbn):
+        print("ISBN no válido")
+        input("Pulsa Enter para continuar.")
+        return
+
+    prestamo = next((p for p in prestamos if p["nie"] == nie and p["isbn"] == isbn), None)
+
+    if not prestamo:
+        print("No se encontró ningún prestamos con esos datos")
+        input("Pulsa Enter para continuar.")
+        return
+    
+    print(f"\nPréstamo encontrado:\n{prestamo}")
+    nueva_fecha = input(f"Nueva fecha de devolucion [{prestamo['fecha_devolucion']}]:").strip()
+    if nueva_fecha:
+        from utils.verificadores import verificar_fecha
+        if verificar_fecha(nueva_fecha):
+            prestamo["fecha_devolucion"] = nueva_fecha
+        else:
+            print("La fecha no es valida por lo que se mantendrá la original")
+
+
+    nuevo_estado = input(f"Nuevo estado (P: Prestado, D: Devuelto) [{prestamo['estado']}]: ").strip().upper()
+    if nuevo_estado in ["P", "D"]:
+        prestamo["estado"] = nuevo_estado
+    
+    DataManager.guardar(prestamos, "prestamos")
+    print("Datos actualizados correctamente")
+    input("Pulsa Enter para continuar.")
+
+
+
+def eliminar_prestamo():
+    prestamos = DataManager.cargar("prestamos")
+    nie = input("NIE del alumno: ").strip().upper()
+    if not validar_nie(nie):
+        print("NIE no válido")
+        input("Pulsa Enter para continuar.")
+        return
+    
+    isbn = input("ISBN del libro: ").strip()
+    if not validar_isbn(isbn):
+        print("ISBN no válido")
+        input("Pulsa Enter para continuar.")
+        return
+
+    prestamo = next((p for p in prestamos if p["nie"] == nie and p["isbn"] == isbn), None)
+
+    if not prestamo:
+        print("No se encontró ningún prestamos con esos datos")
+        input("Pulsa Enter para continuar.")
+        return
+    
+    print(f"\nPréstamo encontrado:\n{prestamo}")
+    print(f" NIE: {prestamo['nie']}, ISBN: {prestamo['isbn']}, Estado: {prestamo['estado']}")
+    confirmar = input("¿Seguro desea eliminar este prestamo? (S/N)").strip().upper()
+    if confirmar == "S":
+        prestamos.remove(prestamo)
+        DataManager.guardar(prestamos, "prestamos")
+        print("Préstamo eliminado correctamente")
+    else:
+        print("Operacion cancelada")
+    input("Pulsa Enter para continuar.")
+
+
+
+def cerrar_prestamo():
+    prestamos = DataManager.cargar("prestamos")
+    nie = input("NIE del alumno: ").strip().upper()
+    if not validar_nie(nie):
+        print("NIE no válido")
+        input("Pulsa Enter para continuar.")
+        return
+    
+    prestamos_alumno = [p for p in prestamos if p["nie"] == nie]
+
+    if not prestamos_alumno:
+        print("Este alumno no tiene prestamos registrados")
+    else:
+        no_devueltos = [p for p in prestamos_alumno if p["estado"] != "D"]
+
+        if no_devueltos:
+            print("No se puede cerrar el prestamo porque aún hay libros pendientes de devolver")
+            for p in no_devueltos:
+                print(f" ISBN: {p['isbn']}  |  Estado: {p['estado']}")
+        else:
+            print("Todos los libros han sido devueltos, puedes cerrar el prestamo")
+    input("Pulsa Enter para continuar.")
+
+
+def firmar_contrato():
+    nie = input("NIE del alumno: ").strip().upper()
+    if not validar_nie(nie):
+        print("NIE no válido")
+        input("Pulsa Enter para continuar.")
+        return
+    
+    prestamos = DataManager.cargar("prestamos")
+    prestamos_nie = [p for p in prestamos if p["nie"] == nie]
+
+    if not prestamos_nie:
+        print("Este alumno no tiene prestamos registrados")
+        input("Pulsa Enter para continuar.")
+        return
+    
+    ruta_base = os.path.dirname(os.path.abspath(__file__))
+    ruta_data = os.path.join(ruta_base, "data")
+    os.makedirs(ruta_data, exist_ok=True)
+
+    ruta_contrato = os.path.join(ruta_data, f"contrato_{nie}.txt")
+
+    with open(ruta_contrato, "w", encoding="utf-8") as f:
+        f.write("CONTRATO DE PRESTAMO DE LIBROS\n")
+        f.write(f"NIE del alumno: {nie}\n")
+        f.write("Listado de libros: \n\n")
+        for p in prestamos_nie:
+            estado = "Devuelto" if p["estado"] == "D" else "Prestado"
+            f.write(f"ISBN: {p['isbn']}  |  Entrega: {p['fecha_entrega']}  |  Devolución:  {p['fecha_devolucion']}  |  Estado: {estado}\n")
+        
+        f.write("\nFirma del alumno: ______________\n")
+        f.write("Firma del centro: ______________\n")
+    
+    print(f"Contrato guardado en {ruta_contrato}")
     input("Pulsa Enter para continuar.")
