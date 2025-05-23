7b12c08c862a50ad5ec308489593749a84850c20
frxxn18 <franpado8@gmail.com>
2025-05-23 13:10:05 +0200

    test_finalizar

--- main_tests.py
+++ main_tests.py
@@ -1,11 +1,11 @@
 import unittest
 
 def main():
+    print(">>> Ejecutando tests...")
     carga = unittest.TestLoader()
     carpeta = carga.discover("tests")
-
     run = unittest.TextTestRunner()
     run.run(carpeta)
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
